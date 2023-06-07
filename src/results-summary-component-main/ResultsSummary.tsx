293fc4141a4c85d0c9b53c208df88245cf9711e7
jfpaezl <108756151+jfpaezl@users.noreply.github.com>
2023-06-07 10:07:28 -0500

    primer desafio

--- src/results-summary-component-main/ResultsSummary.tsx
+++ src/results-summary-component-main/ResultsSummary.tsx
@@ -1,33 +1,30 @@
 import styled from "styled-components";
 import Layouth from "../components/layouth/Layouth";
 import Skills from "./components/Skills";
+import datos from "./data.json";
 
 export default function ResultsSummary() {
   return (
-    <ResultsSummaryCss>
-      <Layouth>
+    <Layouth>
+      <ResultsSummaryCss>
         <section>
-          <div>
+          <div className="container">
             <h3>Your Result</h3>
-            <div>
-              <p>
+            <div className="container2">
+              <p className="circulo">
                 76 <span>of 100</span>
               </p>
-              <p>Great</p>
-              <span>
+              <p className="great">Great</p>
+              <span className="spanText">
                 You scored higher than 65% of the people who have taken these
                 tests.
               </span>
             </div>
           </div>
-          <div>
+          <div className="containerSkills">
             <strong>Summary</strong>
-            <div>
-              <Skills text="Reaction" puntos={80} img="src/results-summary-component-main/assets/images/icon-reaction.svg"
-              />
-              <Skills text="Memory" puntos={92} img="src/results-summary-component-main/assets/images/icon-memory.svg"/>
-              <Skills text="Verbal" puntos={61} img="src/results-summary-component-main/assets/images/icon-verbal.svg"/>
-              <Skills text="Visual" puntos={72} img="src/results-summary-component-main/assets/images/icon-visual.svg"/>
+            <div className="containerSkills2">
+              <Skills datos={datos} />
             </div>
             <button>Continue</button>
           </div>
@@ -39,17 +36,113 @@ export default function ResultsSummary() {
           </a>
           . Coded by <a href="#">Your Name Here</a>.
         </div>
-      </Layouth>
-    </ResultsSummaryCss>
+      </ResultsSummaryCss>
+    </Layouth>
   );
 }
 
 const ResultsSummaryCss = styled.div`
-  .attribution {
-    font-size: 11px;
-    text-align: center;
+  height: 100vh;
+  font-size: 1.5rem;
+  @font-face {
+    font-family: "MiFuenteLocal";
+    src: url("src/results-summary-component-main/assets/fonts/HankenGrotesk-VariableFont_wght.ttf");
   }
-  .attribution a {
-    color: hsl(228, 45%, 44%);
+  font-family: "MiFuenteLocal";
+  background-color: hsl(0, 0%, 100%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  section {
+    max-width: 50rem;
+    display: flex;
+    border-radius: 1rem;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.062);
+    @media (max-width: 768px) {
+      flex-direction: column;
+    }
+  }
+  .container {
+    background-image: linear-gradient(
+      to bottom,
+      hsl(252, 100%, 67%),
+      hsl(241, 81%, 54%)
+    );
+    width: 25rem;
+    height: 35rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1rem;
+    gap: 2rem;
+    padding: 3rem;
+    color: hsl(241, 100%, 89%);
+
+    .container2 {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+      text-align: center;
+    }
+    .circulo {
+      font-size: 5rem;
+      font-weight: bold;
+      color: hsl(0, 0%, 100%);
+      border-radius: 50%;
+      width: 13rem;
+      height: 13rem;
+      background-color: white;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      background-image: linear-gradient(
+        to bottom,
+        hsla(256, 72%, 46%, 1),
+        hsl(241, 81%, 54%),
+        hsl(241, 81%, 60%)
+      );
+      span {
+        font-size: 1.2rem;
+        color: hsla(241.07142857142856, 100%, 89.01960784313725%, 0.534);
+      }
+    }
+    .great {
+      font-size: 2rem;
+      font-weight: bold;
+      color: hsl(0, 0%, 100%);
+      margin-top: 1rem;
+    }
+    .spanText {
+      font-size: 1.3rem;
+    }
+  }
+
+  .containerSkills {
+    width: 25rem;
+    height: 35rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border-radius: 1rem;
+    gap: 2rem;
+    padding: 3rem;
+    .containerSkills2 {
+      width: 100%;
+    }
+    button {
+      width: 100%;
+      padding: 1rem;
+      border-radius: 2rem;
+      background-color: hsl(224, 30%, 27%);
+      color: hsl(241, 100%, 89%);
+      font-size: 1.3rem;
+      font-weight: bold;
+      border: none;
+      cursor: pointer;
+    }
   }
 `;
--- src/results-summary-component-main/components/Skills.tsx
+++ src/results-summary-component-main/components/Skills.tsx
@@ -1,23 +1,52 @@
 import styled from "styled-components";
 
-export default function Skills({text = '', puntos = 0, img = ''}) {
+export default function Skills({ datos = {} }) {
   return (
     <SkillsCss>
-      <div className="skills">
-        <div className="textSkill">
-          <img
-            src={img} alt="iconos"/>
-          <p>{text}</p>
+      {datos.map((dat, index) => (
+        <div
+          key={index}
+          className="skills"
+          style={{ backgroundColor: dat.Bcolor }}
+        >
+          <div className="textSkill">
+            <img src={dat.icon} alt="iconos" />
+            <p style={{ color: dat.Ctext }}>{dat.category}</p>
+          </div>
+          <p className="puntos">
+            {dat.score} <span>/ 100</span>
+          </p>
         </div>
-        <p className="puntos">
-          {puntos} <span>/ 100</span>
-        </p>
-      </div>
+      ))}
     </SkillsCss>
   );
 }
 
 const SkillsCss = styled.div`
-  background-color: hsl(0, 100%, 67%);
-
-`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  .skills {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: .8rem;
+    border-radius: 0.5rem;
+    font-size: 1.3rem;
+    .puntos{
+      font-weight: bold;
+    }
+    span{
+      font-size: 1.1rem ;
+      font-weight: normal;
+      color: hsl(241, 100%, 89%);
+    }
+  }
+  img {
+    width: 1.5rem;
+  }
+  .textSkill {
+    display: flex;
+    gap: 1rem;
+  }
+`;
